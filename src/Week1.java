100c38c48c401b2dd375600c34372c248970920d
Nick Onate <nick@Nicks-MacBook-Air.local>
2021-01-21 17:29:18 -0700

    created week 1 homework

---
+++ src/Week1.java
@@ -0,0 +1,28 @@
+
+public class Week1 {
+
+	public static void main(String[] args) {
+		int itemprice = 24;
+		System.out.println(itemprice);
+		int moneywallet = 26;
+		System.out.println(moneywallet);
+		int friends = 40; 
+		System.out.println(friends);
+		int nicksage = 20;
+		System.out.println(nicksage);
+		char middlei = 'P';
+		System.out.println(middlei);
+		String firstname = "Nick";
+		System.out.println(firstname);
+		String lastname = "Onate";
+		System.out.println(lastname);
+		String fullname = firstname + " " + middlei + " " + lastname;
+		System.out.println(fullname);
+		int friendyear = friends / nicksage;
+		System.out.println(friendyear);
+		int remainingmoney = moneywallet - itemprice;
+		System.out.println(remainingmoney);
+			
+	}
+
+}
